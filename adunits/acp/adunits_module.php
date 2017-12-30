d42e994d3123eac79d09ba9fce6aa8a5f33e57cd
Daniel Mota <leinad4mind@gmail.com>
2017-12-30 20:13:55 +0000

    Spaces and Comments

--- adunits/acp/adunits_module.php
+++ adunits/acp/adunits_module.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
 *
 * @package Ad Units
-* @copyright BB3.MOBi (c) 2015 Anvar http://apwa.ru
-* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* @copyright BB3.Mobi 2015 (c) Anvar(http://apwa.ru)
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 */
 
